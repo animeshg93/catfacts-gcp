23e52ad8290c1e4dfa268083f634623be73a6f97
Animesh Gandhi <animeshg93@gmail.com>
2026-03-04 20:59:18 -0600

    Going back to Square 0

--- src/main/java/com/example/purrfacts/cat/service/TokenService.java
+++ src/main/java/com/example/purrfacts/cat/service/TokenService.java
@@ -21,9 +21,11 @@ public class TokenService {
   public String fetchAccessToken() {
     try {
       return Unirest.post("https://dev-xjtz6ps1llg7ich0.us.auth0.com/oauth/token")
-              .header("content-type", "application/json")
-              .body("{\"client_id\":\"70jBOJCZ9M0wqa1DjwuSbeSIgFMKOSia\",\"client_secret\":\"fh8Vo2UjA0WMLn-n2TLRtUDe-45b12I2QMHJMn-GOlobRb19SXRc_eb22eaE4h_f\",\"audience\":\"https://cat-facts.com/api/v2/\",\"grant_type\":\"client_credentials\"}")
-              .asString().getBody();
+          .header("content-type", "application/json")
+          .body(
+              "{\"client_id\":\"70jBOJCZ9M0wqa1DjwuSbeSIgFMKOSia\",\"client_secret\":\"fh8Vo2UjA0WMLn-n2TLRtUDe-45b12I2QMHJMn-GOlobRb19SXRc_eb22eaE4h_f\",\"audience\":\"https://cat-facts.com/api/v2/\",\"grant_type\":\"client_credentials\"}")
+          .asString()
+          .getBody();
 
     } catch (Exception e) {
       throw new RuntimeException("Failed to fetch access token: " + e.getMessage(), e);
