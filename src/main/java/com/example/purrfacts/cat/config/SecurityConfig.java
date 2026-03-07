40a75bbf1c897ab8662577d9e40de3abec9c26a1
Animesh Gandhi <animeshg93@gmail.com>
2026-03-07 02:46:58 -0600

    Disabled csrf

--- src/main/java/com/example/purrfacts/cat/config/SecurityConfig.java
+++ src/main/java/com/example/purrfacts/cat/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,11 +14,11 @@ public class SecurityConfig {
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    String[] authenticatedPatterns = new String[] {"/actuator/**", "/auth/**", "/api/message/push"};
 
-    http.authorizeHttpRequests(
+    http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(
             auth ->
-                auth.requestMatchers(authenticatedPatterns)
+                auth.requestMatchers("/actuator/**", "/auth/**", "/api/message/push")
                     .permitAll()
                     .anyRequest()
                     .authenticated())
